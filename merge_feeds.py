61d38f7489c27613bb2925237ffc407d80caf9ef
Fahim <49230752+evilgodfahim@users.noreply.github.com>
2025-12-15 13:00:17 +0600

    Update merge_feeds.py

--- merge_feeds.py
+++ merge_feeds.py
@@ -27,7 +27,7 @@ CUTOFF = datetime.now(timezone.utc) - timedelta(hours=48)
 BLOCKED_PATHS = (
     "/sport/",
     "/entertainment/",
-    "/culture/","/food/","/music/","/tv-and-tadio/","/lifeandstyle/","/stage/","/film/","/artanddesign/","/fashion/","/books/","/football/","/media/","/travel/","/society/","/uk-news/","/australia-news/"
+    "/culture/","/food/","/music/","/tv-and-tadio/","/lifeandstyle/","/stage/","/film/","/artanddesign/","/fashion/","/books/","/football/","/media/","/travel/","/society/","/uk-news/","/australia-news/","/television/"
 
 )
 

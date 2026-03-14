c895ef9936ff5b88febb2fa37c5cd6e8b5f942d3
Fahim <49230752+evilgodfahim@users.noreply.github.com>
2026-03-14 17:38:25 +0600

    Update merge_feeds.py

--- merge_feeds.py
+++ merge_feeds.py
@@ -16,7 +16,9 @@ FEEDS = [
     "https://www.theguardian.com/international/rss",
     "https://feeds.guardian.co.uk/theguardian/world/rss",
     "https://www.theguardian.com/us/commentisfree/rss",
-"http://www.guardian.co.uk/weekly/rss"
+"http://www.guardian.co.uk/weekly/rss",
+
+"http://www.guardian.co.uk/environment/renewableenergy/rss"
 ]
 
 OUTPUT = "merged.xml"
